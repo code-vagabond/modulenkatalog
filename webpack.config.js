45370190328bde2edfc93846a223efbf4e06a104
code-vagabond <phung.linh2010@gmail.com>
2017-04-15 14:09:36 +0200

    colorized semester indicator

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,26 @@
 var webpack = require('webpack');
 var path = require('path');
 
-var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
-var APP_DIR = path.resolve(__dirname, 'src/client/app');
+var BUILD_DIR = path.resolve(__dirname, 'js');
+// var APP_DIR = path.resolve(__dirname, 'src/client/app');
 
 var config = {
-  entry: APP_DIR + '/index.jsx',
+  entry: './js/index.js',
   output: {
     path: BUILD_DIR,
     filename: 'bundle.js'
+  },
+  watch:true,
+  module: {
+  	loaders: [
+  		{
+  			test: /jsx?$/,
+  			loader: 'babel-loader',
+  			exclude: /node_modules/,
+  			query: {
+  				presets: ['es2015','react']
+  			}
+  		}]
   }
 };
 
