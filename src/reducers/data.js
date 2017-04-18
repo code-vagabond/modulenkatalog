16f8f8154e2e2ab70e589b6a4a39f0f087aa4b9c
code-vagabond <phung.linh2010@gmail.com>
2017-04-18 22:44:30 +0200

    Revert "Revert "Merge branch 'reactjs'""
    This reverts commit f5769ff5fe03f1e9b3553cdd7a996755663c44d2.

---
+++ src/reducers/data.js
@@ -0,0 +1,9 @@
+export default function() {
+  return [
+    { module_name: 'Javascript: The Good Parts', cp_number: 10 },
+    { module_name: 'Harry Potter', cp_number: 8 },
+    { module_name: 'The Dark Tower', cp_number: 5 },
+    { module_name: 'Eloquent Ruby', cp_number: 7 }
+  ];
+}
+
